b57d75ffc5da134146e0ecea39419ca0bdb55247
Vikram Patil <vikram.vvp@gmail.com>
2018-01-02 23:57:15 +0530

    react icons used

--- frontend/src/components/PostsList.js
+++ frontend/src/components/PostsList.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import { loadPosts } from "../actions";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import DarkThumbsUp from 'react-icons/lib/fa/thumbs-up';
+import DarkComments from 'react-icons/lib/fa/comments';
 
 class PostsList extends Component {
   componentDidMount() {
@@ -18,12 +20,11 @@ class PostsList extends Component {
         {_.map(this.props.posts, post => (
         <div className="card mb-4" key={post.id}>
           <div className="card-body">
-            <h2 className="card-title">{post.title}</h2>
+            <Link to={`/post?id=${post.id}/`}><h2 className="card-title">{post.title}</h2></Link>
             <p className="card-text">{post.body.substr(0, 150) + '...'}</p>
-            <Link className="btn btn-primary" to={`/post?id=${post.id}/`}>Read More &rarr;</Link>
           </div>
           <div className="card-footer text-muted">
-            Posted on January 1, 2017 by {post.author}
+            Posted on January 1, 2017 by {post.author}&nbsp;&nbsp;&#124;&nbsp;&nbsp;<DarkComments b size={20} /> {post.commentCount}&nbsp;&nbsp;&#124;&nbsp;&nbsp;<DarkThumbsUp size={20}/> {post.voteScore}
           </div>
         </div>
       ))}
