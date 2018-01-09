8ff68cb36ad3979fbb8868c59652c14d0abe8e75
Vikram Patil <vikram.vvp@gmail.com>
2018-01-09 21:38:40 +0530

    implemented review 3 suggestion

--- frontend/src/components/DefaultView.js
+++ frontend/src/components/DefaultView.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PostActions from './PostActions';
 import ShowCategories from '../containers/ShowCategories';
 import VisiblePostsList from '../containers/VisiblePostsList';
 
-class DefaultView extends Component {
-  render() {
-    return (
-      <div className="row">
-        <VisiblePostsList />
-        <div className="col-md-3">
-          <PostActions clickItems={['add']} />
-          <ShowCategories />
-        </div>
-      </div>
-    );
-  }
-}
+const DefaultView = () => (
+  <div className="row">
+    <VisiblePostsList />
+    <div className="col-md-3">
+      <PostActions clickItems={['add']} />
+      <ShowCategories />
+    </div>
+  </div>
+)
 
 export default DefaultView;
--- frontend/src/components/PostActions.js
+++ frontend/src/components/PostActions.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 
-class PostActions extends React.Component {
-  render() {
-    let { clickItems, onDeletePost } = this.props;
-    return (
-      <div className="card my-4">
-        <h5 className="card-header">Actions</h5>
-        <div className="card-body">
-          <div className="btn-group-vertical">
-            {_.indexOf(clickItems, 'list') !== -1 ?
-              <Link className="btn btn-secondary" to='/all' >Back to list</Link> : ""}
-            {_.indexOf(clickItems, 'add') !== -1 ?
-              <Link className="btn btn-secondary" to='/posts/new' >Add New Post</Link> : ""}
-            {_.indexOf(clickItems, 'edit') !== -1 ?
-            <Link className="btn btn-secondary" to={`/posts/edit/${this.props.match.params.id}`} >Edit Post</Link> : ""}
-            {_.indexOf(clickItems, 'delete') !== -1 ?
-              <a className="btn btn-secondary" href="" role="button"
-                onClick={(e) => { e.preventDefault(); onDeletePost('postactions',this.props.match.params.id) }}>
-                Delete Post</a> : ""}
-          </div>
-        </div>
+const PostActions = ({ clickItems, onDeletePost, match }) => (
+  <div className="card my-4">
+    <h5 className="card-header">Actions</h5>
+    <div className="card-body">
+      <div className="btn-group-vertical">
+        {_.indexOf(clickItems, 'list') !== -1 ?
+          <Link className="btn btn-secondary" to='/all' >Back to list</Link> : ""}
+        {_.indexOf(clickItems, 'add') !== -1 ?
+          <Link className="btn btn-secondary" to='/posts/new' >Add New Post</Link> : ""}
+        {_.indexOf(clickItems, 'edit') !== -1 ?
+          <Link className="btn btn-secondary" to={`/posts/edit/${match.params.id}`} >Edit Post</Link> : ""}
+        {_.indexOf(clickItems, 'delete') !== -1 ?
+          <a className="btn btn-secondary" href="" role="button"
+            onClick={(e) => { e.preventDefault(); onDeletePost('postactions', match.params.id) }}>
+            Delete Post</a> : ""}
       </div>
-    )
-  }
-}
+    </div>
+  </div>
+)
 
 PostActions.propTypes = {
   clickItems: PropTypes.array.isRequired,
